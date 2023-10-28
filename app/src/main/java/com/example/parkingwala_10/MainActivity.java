1d56de7f30ae1e9d4b0f69d606e6889e1bbeb835
ParkingWala <1parkingwala@gmail.com>
2023-10-28 21:42:29 +0530

    Button Added

--- app/src/main/java/com/example/parkingwala_10/MainActivity.java
+++ app/src/main/java/com/example/parkingwala_10/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.parkingwala_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeActivity();
+        onClick();
     }
+
+    private void initializeActivity() {
+        textView = findViewById(R.id.buttonDemo);
+    }
+
+    private void onClick() {
+        textView.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, R.string.parkingwala, Toast.LENGTH_LONG).show();
+        });
+    }
+
 }
